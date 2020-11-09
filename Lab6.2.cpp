7f10e6d96cf657a9300f4b72be822c19e83205e2
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-09 14:26:56 +0200

    Initial commit

---
+++ Lab6.2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+void Create(int* c, const int size, int min, int max, int i) {
+	c[i] = min + rand() % (max - min + 1);
+	if (i < size - 1) {
+		Create(c, size, min, max, i + 1);
+	}
+}
+
+void arrPrint(int* c, const int size, int i) {
+	cout << setw(4) << c[i];
+	if (i < size - 1)
+		arrPrint(c, size, i + 1);
+	else
+		cout << endl;
+}
+
+int arrMaxMin(int* c, const int size, int i, int max, int min, int indexMax, int indexMin) {
+	if (c[i] > max) {
+		max = c[i];
+		indexMax = i;
+	}
+	if (c[i] < min) {
+		min = c[i];
+		indexMin = i;
+	}
+	if (i < size - 1) {
+		arrMaxMin(c, size, i + 1, max, min, indexMax, indexMin);
+	}
+	else {
+		cout << indexMax << "  " << indexMin << endl;
+		return 0;
+	}
+}
+
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int a = 15;
+
+	int c[a];
+
+	int Low = 5;
+	int High = 90;
+
+	Create(c, a, Low, High, 0);
+	arrPrint(c, a, 0);
+	arrMaxMin(c, a, 1, c[0], c[0], 0, 0);
+
+
+	system("pause");
+	cin.get();
+	return 0;
+}
---
+++ UnitTest6.2/UnitTest6.2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62
+{
+	TEST_CLASS(UnitTest62)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int a = 2;
+			int c[a] = { 1, 1 };
+			t = arrMaxMin(c, a, 1, c[0], c[0], 0, 0);
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
